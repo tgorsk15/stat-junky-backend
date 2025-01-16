140e546d34c903004ca336b27d4b8d6445687c05
tgorsk15 <tgbusiness463@gmail.com>
2025-01-15 19:56:26 -0600

    Connect backend to API and API key

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,11 @@
+const { BalldontlieAPI } = require("@balldontlie/sdk")
 
+
+const api = new BalldontlieAPI({ apiKey: process.env.API_KEY })
 
 
 exports.homeTest = async (req, res) => {
+    const teams = await api.nba.getTeams()
+    console.log('here is teams:', teams)
     res.send('Hello Friend')
 }
