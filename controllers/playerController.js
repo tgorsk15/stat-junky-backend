7035e890e690e4e26105466962af3d8b344335f6
tgorsk15 <tgbusiness463@gmail.com>
2025-01-18 14:11:42 -0600

    Successfully get 200 response from API on player search

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -1,9 +1,26 @@
+const { BalldontlieAPI } = require("@balldontlie/sdk")
+const api = new BalldontlieAPI({ apiKey: process.env.API_KEY })
+
 const apiUrl = process.env.API_URL
 
 exports.getPlayerBySearch = async (req, res) => {
     const playerName = req.query.search
     console.log('search query:', playerName)
-    const players = await fetch(`${apiUrl}/players/${playerName}`)
-    console.log('here is players with that name:', players)
-    res.send('Hi Anthony')
+
+    try {
+        const options = {
+            method: "GET",
+            headers: {"Content-Type": "application/json", "Authorization": process.env.API_KEY},
+        }
+
+        const response = await fetch(`${apiUrl}/players?search=${playerName}`, options)
+        const players = await response.json()
+
+        console.log('here is players with that name:', players)
+        res.send('Hi Anthony')
+    } catch {
+        console.error('Error fetching player:', error)
+        res.status(500).json({ error: 'Failed to fetch player data' })
+    }
+    
 }
