04e47dfcf53bf4876eb056543cf0c59e5958286e
tgorsk15 <tgbusiness463@gmail.com>
2025-03-01 09:39:45 -0600

    Modify app.js to accomadate for AWS Lambda

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const serverless = require("serverless-http")
 const cors = require("cors");
 
 require('dotenv').config();
@@ -17,15 +18,33 @@ app.use(
         ]
     })
 )
+console.log('environment', process.env.HOME)
+const basePath = process.env.HOME !== '/Users/Tyler' ? '/prod' : '';
 
 const homeRouter = require('./routes/home')
 const playerRouter = require('./routes/player')
 
 
 //connect routers:
-app.use("/", homeRouter)
-app.use("/player", playerRouter)
+
+// old:
+// app.use("/", homeRouter)
+// app.use("/player", playerRouter)
+
+app.use(`${basePath}/`, homeRouter)
+app.use(`${basePath}/player`, playerRouter)
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the API' });
+});
+
+if (process.env.HOME === '/Users/Tyler') {
+    const PORT = process.env.PORT || 5000
+    app.listen(PORT, () => console.log(`always watchin you on ${PORT}`))
+}
 
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`always watchin you on ${PORT}`))
+
+module.exports.handler = serverless(app, {
+    basePath: '/prod'
+})
