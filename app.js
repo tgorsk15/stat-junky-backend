bfa867d944be55f35f636f2de937607097aeff96
tgorsk15 <tgbusiness463@gmail.com>
2025-03-02 10:31:16 -0600

    Pull back on Lambda updates, prep app.js for AWS EC2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const serverless = require("serverless-http")
+// const serverless = require("serverless-http")
 const cors = require("cors");
 
 require('dotenv').config();
@@ -18,8 +18,8 @@ app.use(
         ]
     })
 )
-console.log('environment', process.env.HOME)
-const basePath = process.env.HOME !== '/Users/Tyler' ? '/prod' : '';
+
+// const basePath = process.env.HOME !== '/Users/Tyler' ? '/prod' : '';
 
 const homeRouter = require('./routes/home')
 const playerRouter = require('./routes/player')
@@ -28,23 +28,21 @@ const playerRouter = require('./routes/player')
 //connect routers:
 
 // old:
-// app.use("/", homeRouter)
-// app.use("/player", playerRouter)
+app.use("/", homeRouter)
+app.use("/player", playerRouter)
 
-app.use(`${basePath}/`, homeRouter)
-app.use(`${basePath}/player`, playerRouter)
+// app.use(`${basePath}/`, homeRouter)
+// app.use(`${basePath}/player`, playerRouter)
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the API' });
 });
 
-if (process.env.HOME === '/Users/Tyler') {
-    const PORT = process.env.PORT || 5000
-    app.listen(PORT, () => console.log(`always watchin you on ${PORT}`))
-}
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`always watchin you on ${PORT}`))
 
 
 
-module.exports.handler = serverless(app, {
-    basePath: '/prod'
-})
+// module.exports.handler = serverless(app, {
+//     basePath: '/prod'
+// })
